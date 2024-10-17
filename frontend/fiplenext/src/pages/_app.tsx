490d553c0c5275f37faa9a2e2990db70084163a4
JunyaOkada <fko2347022@stu.o-hara.ac.jp>
2024-10-17 14:12:17 +0900

    header　バーを作るだけ

--- frontend/fiplenext/src/pages/_app.tsx
+++ frontend/fiplenext/src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@styles/styles/globals.css";
 import type { AppProps } from "next/app";
-
+import Head from 'next/head'
+import Image from "next/image";
+import Link from 'next/link'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <>
+    <Head>
+        <div className="flex justify-center border-b">
+        <Link href="/" passHref>
+        <Image className="mt-[10px] mb-[10px]" src="/fipleheader.png" alt="fipleheader" width="200" height="80" />
+        </Link>
+        </div>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  );
 }
