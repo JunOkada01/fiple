487565373b522e35f44098ccaa41b18c0afa6964
Marin <fko2347009@stu.o-hara.ac.jp>
2024-10-28 10:01:54 +0900

    まりん一応コミット

--- frontend/fiplenext/src/components/Navigation.tsx
+++ frontend/fiplenext/src/components/Navigation.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserLarge } from '@fortawesome/free-solid-svg-icons';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+// Regularスタイルのアイコンをインポート
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+// Cartアイコンはsolid版を使用
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,24 +11,24 @@ const Navigation: React.FC = () => {
     return (
         <nav className="flex justify-center items-center gap-6 py-5">
             <div className="relative max-w-[250px]">
-            <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
-                <FontAwesomeIcon icon={faMagnifyingGlass} className='text-[20px]' />
-            </span>
-            <input
-                type="text"
-                placeholder=""
-                className="w-full border-b border-black px-4 py-1 pl-10"
-            />
+                <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className="text-[20px]" />
+                </span>
+                <input
+                    type="text"
+                    placeholder=""
+                    className="w-full border-b border-black px-4 py-1 pl-10"
+                />
             </div>
             <div className="flex gap-6">
                 <Link href="/accounts/login" className="hover:text-gray-400">
-                    <FontAwesomeIcon icon={faUserLarge} className='text-[20px]'/>
+                    <FontAwesomeIcon icon={faUser} className="text-[20px]" />
                 </Link>
                 <Link href="/favorites" className="hover:text-gray-400">
-                    <FontAwesomeIcon icon={faHeart} className='text-[20px]' />
+                    <FontAwesomeIcon icon={faHeart} className="text-[20px]" />
                 </Link>
                 <Link href="/cart" className="hover:text-gray-400">
-                    <FontAwesomeIcon icon={faCartShopping} className='text-[20px]' />
+                    <FontAwesomeIcon icon={faCartShopping} className="text-[20px]" />
                 </Link>
             </div>
         </nav>
