2b7bb1dc4a293aa708b5e0214bb50921b7fdf3d4
Marin <fko2347009@stu.o-hara.ac.jp>
2024-10-30 14:13:59 +0900

    商品フィルターの共通コンポーネント化

--- frontend/fiplenext/src/pages/favorites/index.tsx
+++ frontend/fiplenext/src/pages/favorites/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Favorites: React.FC = () => {  
     return (  
         <div className="container mx-auto max-w-screen-xl px-4">  
-        <h1 className="text-3xl font-bold text-center my-8">YOUR LIKED ITEMS</h1>  
+        <h1 className="text-3xl text-center my-8">YOUR LIKED ITEMS</h1>  
         
         <div className="flex flex-col items-center">  
             <div className="grid grid-cols-5 gap-4 max-w-full">  
