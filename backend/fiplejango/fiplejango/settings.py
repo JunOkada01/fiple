ee4ea14a4136df5fdd9bfc522fa8fce11f4c3bd0
JunyaOkada <fko2347022@stu.o-hara.ac.jp>
2024-11-08 11:15:59 +0900

    パスワード隠しました

--- backend/fiplejango/fiplejango/settings.py
+++ backend/fiplejango/fiplejango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,14 +168,16 @@ AUTHENTICATION_BACKENDS = [
 
 
 
-# メール設定
+# .envファイルの読み込み
+load_dotenv()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-mail.outlook.com' # 使用するメールサーバー
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fko2347022@stu.o-hara.ac.jp'
-EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = 'fko2347022@stu.o-hara.ac.jp'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # JWT設定
 # SECRET_KEY = 'your-secret-key'  # すでに設定されているはず
