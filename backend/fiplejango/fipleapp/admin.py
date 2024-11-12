8953bf2470bc987ec930bc6ac0ba6b5130a0bd71
shomaozasa <fko2347047@stu.o-hara.ac.jp>
2024-11-12 15:12:33 +0900

    adminにモデルを追加

--- backend/fiplejango/fipleapp/admin.py
+++ backend/fiplejango/fipleapp/admin.py
@@ -2,12 +2,9 @@
 from django.contrib import admin
 from .models import CustomUser
 from django.contrib import admin
-from.models import QuestionCategory,FAQ
-from .models import Contact, ContactCategory
-
-from .models import *
+from.models import *
 # Register your models here.
-admin.site.register(CustomUser)
+
 @admin.register(QuestionCategory)
 class QuestionCategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -29,8 +26,23 @@ class ContactCategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
+admin.site.register(CustomUser)
 admin.site.register(AdminUser)
 admin.site.register(Category)
 admin.site.register(SubCategory)
+admin.site.register(Color)
+admin.site.register(Size)
+admin.site.register(ProductOrigin)
 admin.site.register(Product)
+admin.site.register(Tag)
+admin.site.register(ProductTag)
 admin.site.register(ProductImage)
+admin.site.register(Cart)
+admin.site.register(Favorite)
+admin.site.register(PaymentMethod)
+admin.site.register(DeliveryAddress)
+admin.site.register(Purchase)
+admin.site.register(PurchaseItem)
+
+
+
