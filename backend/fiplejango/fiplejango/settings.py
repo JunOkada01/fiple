35b05074960edb2bafa6187e277188352fe2473e
JunyaOkada <fko2347022@stu.o-hara.ac.jp>
2025-02-14 11:52:21 +0900

    css反映6戻し

--- backend/fiplejango/fiplejango/settings.py
+++ backend/fiplejango/fiplejango/settings.py
@@ -176,10 +176,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoryFinder',
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
