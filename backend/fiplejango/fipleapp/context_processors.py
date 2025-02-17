02413a620ac3db57844cae698d749c24bce2b11b
JunyaOkada <fko2347022@stu.o-hara.ac.jp>
2025-02-18 01:24:22 +0900

    管理者ページに表示する情報を更新し、フルネームがない場合はユーザー名を使用するように変更

--- backend/fiplejango/fipleapp/context_processors.py
+++ backend/fiplejango/fipleapp/context_processors.py
@@ -1,9 +1,20 @@
 # 全ての管理者ページに管理者名と管理者IDを表示するためのコンテキストプロセッサ
 def admin_info(request):
+    context = {}
     if request.user.is_authenticated:
-        return {
-            'username': request.user.username,  # nameの代わりにusernameを使用
-            # または
-            'name': request.user.get_full_name() or request.user.username,  # フルネームがない場合はusernameを使用
+        context = {
+            # 'name': request.user.get_full_name() or request.user.username,
+            'name': getattr(request.user, 'get_full_name', lambda: request.user.name)(),
+            # 'admin_id': request.user.admin_id
         }
-    return {}
+    return context
+
+# # 全ての管理者ページに管理者名と管理者IDを表示するためのコンテキストプロセッサ
+# def admin_info(request):
+#     if request.user.is_authenticated:
+#         return {
+#             'username': request.user.username,  # nameの代わりにusernameを使用
+#             # または
+#             'name': request.user.get_full_name() or request.user.username,  # フルネームがない場合はusernameを使用
+#         }
+#     return {}
