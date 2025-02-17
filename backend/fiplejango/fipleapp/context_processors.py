2e7ae7c43b234ab07cedd8dffabe0b80f3b05404
JunyaOkada <fko2347022@stu.o-hara.ac.jp>
2025-02-18 03:38:42 +0900

    管理者ページのコンテキストプロセッサを更新し、ユーザータイプに応じて表示情報を変更。CustomUserの場合はユーザー名を、AdminUserの場合は名前とadmin_idを表示するように修正。

--- backend/fiplejango/fipleapp/context_processors.py
+++ backend/fiplejango/fipleapp/context_processors.py
@@ -1,14 +1,32 @@
+from .models import CustomUser, AdminUser
+
 # 全ての管理者ページに管理者名と管理者IDを表示するためのコンテキストプロセッサ
+# def admin_info(request):
+#     context = {}
+#     if request.user.is_authenticated:
+#         context = {
+#             # 'username': request.user.username,  # nameの代わりにusernameを使用
+#             'name': request.user.name,
+#             # 'name': getattr(request.user, 'get_full_name', lambda: request.user.name)(),
+#             'admin_id': request.user.admin_id
+#         }
+#     return context
 def admin_info(request):
     context = {}
     if request.user.is_authenticated:
-        context = {
-            # 'username': request.user.username,  # nameの代わりにusernameを使用
-            'name': request.user.name,
-            # 'name': getattr(request.user, 'get_full_name', lambda: request.user.name)(),
-            'admin_id': request.user.admin_id
-        }
+        if isinstance(request.user, CustomUser):
+            context = {
+                'username': request.user.username,
+
+            }
+        elif isinstance(request.user, AdminUser):
+            context = {
+                'name': request.user.name,
+                'admin_id': request.user.admin_id
+            }
+    
     return context
+
 
 # # 全ての管理者ページに管理者名と管理者IDを表示するためのコンテキストプロセッサ
 # def admin_info(request):
